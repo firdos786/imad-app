735f59e80390d0750afe0fb4029c8db8f19f6f12
firdos786 <36784028+firdos786@users.noreply.github.com>
2018-04-28 16:26:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,15 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-add.gat('/article-one',function(req,res){
+add.get('/article-one',function(req,res){
     res.send('article one is served here');
 
 })
-add.gat('/article-one',function(req,res){
+add.get('/article-one',function(req,res){
     res.send('article one is served here');
 
 })
-add.gat('/article-one',function(req,res){
+add.get('/article-one',function(req,res){
     res.send('article one is served here');
 
 })
