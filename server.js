e186c6bcb5da98031b0bb71119a14d9dd9ecebd5
firdos786 <36784028+firdos786@users.noreply.github.com>
2018-04-28 16:32:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function (req,res){
-  res.snd('hello');
+  res.send('hello');
 });
 
 app.get('/ui/style.css', function (req, res) {
